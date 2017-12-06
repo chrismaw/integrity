d62f9cecdf772e78dc44de5c12e951d696cd5ed2
cmaw <christophermaw@outlook.com>
2017-12-06 17:15:58 -0500

    added sections model and index includes

--- app/Http/Controllers/Site/Index.php
+++ app/Http/Controllers/Site/Index.php
@@ -4,12 +4,19 @@ namespace App\Http\Controllers\Site;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\DB;
+use App\Section;
 
 class Index extends Controller{
 
 	public function index() {
-		$settings = DB::table( 'settings' )->first();
-		$sections = array('about', 'contact', 'download');
+		$settings = DB::table('settings')->first();
+		$sections = DB::table('sections')->where([
+            ['active', '=', 1],
+            ['where_used', 'like', '%index%'],
+                ])->get();
+
+		var_dump($sections);
+
 		return view( 'index', [ 'settings' => $settings, 'sections' => $sections] );
 	}
 
---
+++ app/Http/Controllers/Site/Sections/Sections.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: cmaw
+ * Date: 12/6/2017
+ * Time: 4:07 PM
+ */
+
+namespace App\Http\Controllers\Site\Sections;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\DB;
+
+class Sections extends Controller{
+
+}
