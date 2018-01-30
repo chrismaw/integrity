3432999c20a31ddf0a3684e5ea9c6dd40f54c92b
Chris <christophermaw@outlook.com>
2018-01-30 08:18:07 -0500

    changer anchor font color

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -16,7 +16,9 @@ window.Vue = require('vue');
  */
 
 Vue.component('example-component', require('./components/ExampleComponent.vue'));
+Vue.component('footer-component', require('./components/FooterComponent.vue'));
 
 const app = new Vue({
+    delimiters: ['${', '}'],
     el: '#app'
 });
