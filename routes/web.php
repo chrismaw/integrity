176844f58d918639708f78df33d824da2f73a757
cmaw <christophermaw@outlook.com>
2018-02-26 17:01:25 -0500

    updated sql

--- routes/web.php
+++ routes/web.php
@@ -29,5 +29,8 @@ Route::get('/resources/stories', 'Site\Pages\Stories@index')->name('stories');
 Route::get('/resources/discipleship-seminar', 'Site\Pages\DiscipleshipSeminar@index')->name('discipleship seminar');
 Route::get('/resources/sermons', 'Site\Pages\Sermons@index')->name('sermons');
 
+Route::get('/resources/sermons/{series}', 'Site\Pages\SermonSeries@index')->name('sermon series');
+Route::get('/resources/sermons/{series}/{sermon}', 'Site\Pages\Sermon@index')->name('sermon');
+
 Route::get('/{parent}/{page}', 'Site\Index@pageIndex')->name('basicPage');
 
